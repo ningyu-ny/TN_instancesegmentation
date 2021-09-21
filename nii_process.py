4ad64e90ea0a9785867a49bf296112c69b252507
ningyu-ny <892380871@qq.com>
2021-09-21 21:53:31 +0800

    images_gather

--- nii_process.py
+++ nii_process.py
@@ -1,14 +1,9 @@
 """Nii转png，Nii转dcm，Dcm转png"""
-from matplotlib import pylab as plt
-from nibabel.viewers import OrthoSlicer3D
-from matplotlib import pyplot as plt
 import imageio
-import nibabel as nib
 import numpy as np
 import SimpleITK as sitk
 import os
 import cv2
-import scipy.misc
 
 
 def hu_to_grayscale(volume, hu_min=None, hu_max=None, mn=None, mx=None):
@@ -38,13 +33,10 @@ def hu_to_grayscale(volume, hu_min=None, hu_max=None, mn=None, mx=None):
     return im_volume
 
 
-def array2png(
-    ArrayDicom, dirname, nickname="", trans=(
-        0, 1, 2), size=(
-            512, 512), start=None, end=None):
-    dir = "./" + dirname
-    if (os.path.exists(dir) == False):
-        os.makedirs(dir)
+def array2png(ArrayDicom, dirname, nickname="", trans=(0, 1, 2), size=( 512, 512), start=None, end=None):
+    directory_name = "./" + dirname
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
     ArrayDicom = hu_to_grayscale(ArrayDicom)
 
     ArrayDicom = np.transpose(ArrayDicom, trans)
@@ -58,35 +50,31 @@ def array2png(
 
         if ArrayDicom[:, :, start + i].shape != size:
             # cv2.resize(ArrayDicom[:, :,start + i],(128,128),interpolation=cv2.INTER_CUBIC)
-            cv2.imwrite('%s/%05d%s.jpg' %
-                        (dir, i, nickname), cv2.resize(ArrayDicom[:, :, start +
-                                                                  i], size, interpolation=cv2.INTER_CUBIC))
-            # print("Output:"+'%s/%05d.png'%(dir,i))
-            # cv2.imwrite('%s/%05d_mask.png'%(dir,i), cv2.resize(ArrayDicom_mask[:, :,start + i],(128,128),interpolation=cv2.INTER_CUBIC))
-            # print("Output:"+'%s/%05d_mask.png'%(dir,i))
+            cv2.imwrite('%s/%05d%s.jpg' % (directory_name, i, nickname), cv2.resize(ArrayDicom[:, :, start + i], size,
+                                                                                    interpolation=cv2.INTER_CUBIC))
         else:
-            cv2.imwrite('%s/%05d%s.jpg' %
-                        (dir, i, nickname), ArrayDicom[:, :, start + i])
-            # print("Output:"+'%s/%05d.png'%(dir,i))
-            # cv2.imwrite('%s/%05d_mask.png'%(dir,i), ArrayDicom_mask[:, :,start + i])
-            # print("Output:"+'%s/%05d_mask.png'%(dir,i))
+            cv2.imwrite('%s/%05d%s.jpg' % (directory_name, i, nickname), ArrayDicom[:, :, start + i])
 
 
-def nii2array(Pathnii):
+def nii2array(nii_path):
 
-    img = sitk.ReadImage(Pathnii)
+    img = sitk.ReadImage(nii_path)
     data = sitk.GetArrayFromImage(img)
     # print("模型形状",data.shape)
     spacing = img.GetSpacing()
     origin = img.GetOrigin()
     return data, spacing, origin
 
-# 遍历整个文件夹内的所有nii.gz文件，将子文件夹也遍历
 
-
-def CrossOver(dir, fl):
-    for i in os.listdir(dir):  # 遍历整个文件夹
-        path = os.path.join(dir, i)
+def CrossOver(directory_name: str, fl):
+    """
+    遍历整个文件夹内的所有nii.gz文件，将子文件夹也遍历
+    :param directory_name: 待遍历的文件夹名称
+    :param fl: 列表
+    :return: 含有nii.gz的文件列表
+    """
+    for i in os.listdir(directory_name):  # 遍历整个文件夹
+        path = os.path.join(directory_name, i)
         if os.path.isfile(path) and (
                 i.split('.')[-1]) == 'gz':  # 判断是否为一个文件，排除文件夹
             fl.append(path)
@@ -125,10 +113,13 @@ def nii2png_single(nii_path, IsData=True):
                 (png_path, img_name, j), slice_i)  # 保存
 
 
-def read_niifile(niifilepath):  # 读取niifile文件
-    # img = nib.load(niifilepath)  # 下载niifile文件（其实是提取文件）
-    # img_fdata = img.get_fdata()  # 获取niifile数据
-    ori_data = sitk.ReadImage(nii_path)
+def read_niifile(niifilepath: str):
+    """
+    读取niifile文件
+    :param niifilepath: nii.gz文件的路径：例如: input.nii.gz
+    :return: data
+    """
+    ori_data = sitk.ReadImage(niifilepath)
     data = sitk.GetArrayFromImage(ori_data)
     spacing = ori_data.GetSpacing()
     origin = ori_data.GetOrigin()
@@ -137,7 +128,8 @@ def read_niifile(niifilepath):  # 读取niifile文件
 
 
 
-def save_fig(niifilepath, savepath):  # 保存为图片
+def save_fig(niifilepath, savepath):
+    # 保存为图片
     fdata = read_niifile(niifilepath)  # 调用上面的函数，获得数据
     (b, h, w) = fdata.shape  # 获得数据shape信息：（长，宽，维度-切片数量，第四维）
     for k in range(b):
@@ -147,7 +139,7 @@ def save_fig(niifilepath, savepath):  # 保存为图片
 
 
 if __name__ == '__main__':
-    directory = r"F:\Github_data\Thyroid_nodule_segmentation_Unet_lvyi/data1"  # 文件夹名称
+    directory = r"F:\Github_data\Thyroid_nodule_segmentation_Unet_lvyi/data2"  # 文件夹名称
     filelist = []
     inputlist = []
     masklist = []
@@ -163,25 +155,21 @@ if __name__ == '__main__':
             masklist.append(nii_path)
 
     for i, (imagename, maskname) in enumerate(zip(inputlist, masklist)):
-    # for i,filename in enumerate(inputlist,start=1):
         img_nii_path = imagename
         mask_nii_path = maskname
 
         img_name = os.path.split(img_nii_path)
-        dir_name='train'+'\\'+img_name[0].split('\\')[-1]
+        dir_name = 'train' + '\\'+ 'images'+ '\\' + img_name[0].split('\\')[-1]
         data_array,_,_ = read_niifile(img_nii_path)
         data_array = data_array[:, 117:437, 126:574]  # 裁剪图片 (n,h,w) 其中h，w对应y,x
         array2png(data_array, "%s" % (dir_name), trans=(1, 2, 0), size=(448, 320))
 
+        dir_name = 'train' + '\\'+ 'masks'+ '\\' + img_name[0].split('\\')[-1]
+        mask, _, _ = nii2array(mask_nii_path)
+        mask = mask[:, 117:437, 126:574]        # 裁剪图片 (n,h,w) 其中h，w对应y,x
+        mask[mask != 0] == 255
+        array2png(mask, "%s" % (dir_name), trans=(1, 2, 0), size=(448, 320))
 
-        # img_name = os.path.split(nii_path)  # 分离文件名
-        # img_name = img_name[-1]
-        # img_name = img_name.split('.')
-        # img_name = img_name[0]
 
-    # nii_path=r"./data"
-    # savepath=r"./data/tn"
-    #     if not os.path.exists(savepath):
-    #         os.makedirs(savepath)
-    #     save_fig(nii_path, savepath)
-    # nii2png_single(nii_path)
+
+
